bc0973566cccad974faf9fe47a9319233e5e483e
Eyecue <eyecue@Eyecues-MacBook-Pro.local>
2018-10-15 14:43:01 -0700

    Complete lesson. Have not run yet.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import todoApp from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(todoApp)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
