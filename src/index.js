99f778912dd81bcb649bec098731e5ee68f1a31d
Eyecue <eyecue@Eyecues-MacBook-Pro.local>
2018-10-15 14:50:31 -0700

    Delete boiler readme. Add redux.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 import todoApp from './reducers'
 
 const store = createStore(todoApp)
